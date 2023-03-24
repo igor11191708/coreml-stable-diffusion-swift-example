72f31ad993502d9f4e2ab96bfd2715f02fc1dbb8
Igor <igorshelopaev@gmail.com>
2023-03-24 16:55:04 +0500

    Initial Commit

---
+++ coreml-stable-diffusion-swift-example/coreml_stable_diffusion_swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreml_stable_diffusion_swift_exampleApp.swift
+//  coreml-stable-diffusion-swift-example
+//
+//  Created by Igor on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct coreml_stable_diffusion_swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coreml-stable-diffusion-swift-exampleTests/coreml_stable_diffusion_swift_exampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  coreml_stable_diffusion_swift_exampleTests.swift
+//  coreml-stable-diffusion-swift-exampleTests
+//
+//  Created by Igor on 24.03.2023.
+//
+
+import XCTest
+@testable import coreml_stable_diffusion_swift_example
+
+final class coreml_stable_diffusion_swift_exampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
